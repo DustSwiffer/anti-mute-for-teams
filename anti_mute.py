d742fe6d6906cddc91f591abd6f5bd8f3504f9d3
Dustin van Hal <dvh_1996@hotmail.com>
2020-05-22 21:10:15 +0200

    fine tuning + replaced ImageGrabber with mss

--- anti_mute.py
+++ anti_mute.py
@@ -2,11 +2,13 @@ from pynput.keyboard import Key, Controller
 import keyboard
 import time
 import cv2
-import mss
+from mss import mss
 import numpy
 from ctypes import windll
 from PIL import ImageGrab
 
+sct = mss()
+monitor = sct.monitors[0]
 kb = Controller()
 user32 = windll.user32
 user32.SetProcessDPIAware()
@@ -16,11 +18,22 @@ muted = False
 print("ANTI-mute for teams")
 print("VERSION: v1.0")
 print("AUTHOR: Dustin van Hal (DustSwiffer)")
+print("")
 print("--------------------")
+print("")
 print("cannot been used while working in other window")
+print("")
+print("--------------------")
+print("")
+print("ANTI-mute activate")
+print("")
 
 while True:
-    screen = numpy.asarray(ImageGrab.grab())    
+ 
+    im = numpy.array(sct.grab(monitor))
+    im = numpy.flip(im[:, :, :3], 2)
+    screen = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+ 
     grayScreen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
     targetMuted = cv2.imread("src/images/mute.png", cv2.IMREAD_GRAYSCALE)
     targetUnmuted = cv2.imread("src/images/unmute.png", cv2.IMREAD_GRAYSCALE)
@@ -28,12 +41,11 @@ while True:
     w, h = targetMuted.shape[::-1]
 
     result = cv2.matchTemplate(grayScreen, targetMuted, cv2.TM_CCOEFF_NORMED)
-    loc = numpy.where(result >= 0.9)
+    location = numpy.where(result >= 0.9)
 
-    if loc[0].size > 0:
-        muted = True;
+    if location [0].size > 0:
+        print("You got muted!")
 
-    if muted:
         kb.press(Key.ctrl)
         kb.press(Key.shift)
         kb.press("m")
@@ -44,13 +56,11 @@ while True:
         kb.release(Key.shift)
         kb.release("m")
 
+        print("You got unmuted!")
         muted = False
-
-    for pt in zip(*loc[::-1]):
-        cv2.rectangle(screen, pt,(pt[0] + w, pt[1] + h), (0,255,0), 3)
+        time.sleep(0.05)
 
     cv2.imshow(title, screen)
-
     if cv2.waitKey(25) & keyboard.is_pressed('q'):
         cv2.destroyAllWindows()
         quit()
